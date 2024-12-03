55aeada88458e86c34708a3818e80705e173f14f
SweetS811 <152507500+SweetS811@users.noreply.github.com>
2024-12-03 16:36:45 +0700

    Add files via upload

---
+++ Mei_11_style.js
@@ -0,0 +1,101 @@
+var size = 0;
+var placement = 'point';
+
+var style_Mei_11 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Hasil Tangkap");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 10.000000 && value <= 86.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 579.997],
+                  scale: 0.018965615339389688,
+                  anchor: [5, 5],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/fish_21.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 86.000000 && value <= 100.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 579.997],
+                  scale: 0.03103464328263767,
+                  anchor: [9, 9],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/fish_22.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 100.000000 && value <= 190.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 579.997],
+                  scale: 0.041379524376850225,
+                  anchor: [12, 12],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/fish_23.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 190.000000 && value <= 214.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 579.997],
+                  scale: 0.05344855232009821,
+                  anchor: [15, 15],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/fish_24.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 214.000000 && value <= 940.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 579.997],
+                  scale: 0.0655175802633462,
+                  anchor: [19, 19],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/fish_25.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
